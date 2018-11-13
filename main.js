803f60a2a0acdc73754176d5ef75a3e91a0f5451
tawachan <tawachan@pixiv.com>
2018-11-13 12:11:39 +0900

    setup

--- main.js
+++ main.js
@@ -1,29 +1,3 @@
 $(function() {
-  $(".input").keyup(function(event) {
-    const inputText = event.target.value;
-    if (inputText.length > 20) {
-      alert("20字を超えています")
-    } else {
-      $(".output").text(inputText);
-    }
-  });
-
-  $(".red").click(function(event) {
-    // 子要素から親要素へのイベントの伝播を止める
-    event.stopPropagation();
-    alert("red");
-  })
-
-
-  $(".blue").click(function(event) {
-    // 子要素から親要素へのイベントの伝播を止める
-    event.stopPropagation();
-    alert("blue");
-  })
-
-  $(".yellow").click(function(event) {
-    // 子要素から親要素へのイベントの伝播を止める
-    event.stopPropagation();
-    alert("yellow");
-  })
+  // ここに jquery を書く
 });
